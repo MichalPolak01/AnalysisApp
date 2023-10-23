6f64f357c387b0d877139595836c238663f59d2a
Michal Polak <35229@student.pwsztar.edu.pl>
2023-10-24 00:35:24 +0200

    Poprawienie wykresów i dodanie więcej danych

--- charts.py
+++ charts.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 import pandas as pd
 
 # Funkcja do tworzenia wykresu na podstawie wybranej daty i nazwy wykresu
@@ -10,7 +11,7 @@ import pandas as pd
 chart_titles = [
     "Ambulances In Use Per Hour",
     "Average Ambulance Distance And Time To Reach Firing",
-    "Average Duration Of Incidents Per Hour",
+    # "Average Duration Of Incidents Per Hour",
     "Average Duration Patrols Heading Towards Incidents Per Hour",
     "Average Swat Distance And Time To Reach Firing"
 ]
@@ -49,21 +50,48 @@ def create_plot(selected_date, selected_chart_title):
     fig, ax = plt.subplots()
 
     if selected_chart_title == "Ambulances In Use Per Hour":
-        ax.bar(list(df["simulationTime[s]"]), df["amountOfSolvingAmbulances"])
+        simulation_time = df["simulationTime[s]"] / 3600 -0.5
+        amount_of_solving_ambulances = df["amountOfSolvingAmbulances"]
+
+        # Utworzenie wykresu 
+        ax.bar(simulation_time, amount_of_solving_ambulances, width = 1)
+
+        # Ustawienia osi i etykiet
+        ax.set_xlim(left = 0)  # Minimalna wartość na osi x
+        ax.set_ylim(bottom = 0)  # Minimalna wartość na osi y
+ 
+        ax.xaxis.set_major_locator(MultipleLocator(1))  # Przyrost co 1 jednostkę na osi x
+        ax.yaxis.set_major_locator(MultipleLocator(1))  # Przyrost co 1 jednostkę na osi y
+
         ax.set_title("Ambulances In Use Per Hour")
-        ax.set_xlabel("Time [s]")
+        ax.set_xlabel("Time [h]")
         ax.set_ylabel("Ambulances In Use")
 
     elif selected_chart_title == "Average Ambulance Distance And Time To Reach Firing":
-        ax.plot(df["simulationTime[s]"], df["averageDistanceToReach[m]"], label="Distance [m]")
-        ax.set_title("Average Ambulance Distance And Time To Reach Firing")
-        ax.set_xlabel("Time [s]")
-        ax.set_ylabel("Distance [m]")
-        ax2 = ax.twinx()
-        ax2.plot(df["simulationTime[s]"], df["averageTimeToReach[s]"], color='orange', label="Time [s]")
-        ax2.set_ylabel("Time [s]")
-        fig.legend(loc="upper right")
+        average_time_to_reach = round(df["averageTimeToReach[s]"] / 60, 2)
+        average_distance_to_reach = round(df["averageDistanceToReach[m]"], 2)
 
+        print (average_distance_to_reach)
+        print (average_time_to_reach)
+
+        # Utworzenie wykresu stem
+        ax.stem(average_distance_to_reach, average_time_to_reach, markerfmt='bo', linefmt='b-', basefmt='r-')
+
+        # Ustawienia osi i etykiet
+        ax.set_xlim(left = 0)  # Minimalna wartość na osi x
+        ax.set_ylim(bottom = 0)  # Minimalna wartość na osi y
+
+        ax.xaxis.set_major_locator(MultipleLocator(250))  # Przyrost co 1 jednostkę na osi x
+        ax.yaxis.set_major_locator(MultipleLocator(1))  # Przyrost co 1 jednostkę na osi y
+
+        ax.set_title("Average Ambulance Distance And Time To Reach Firing")
+        ax.set_xlabel("averageDistanceToReach[m]")
+        ax.set_ylabel("averageTimeToReach[min]")
+
+        # Dodanie siatki
+        ax.grid(True)
+
+    # Te dane trzeba przerobić pod inny wykres
     elif selected_chart_title == "Average Duration Of Incidents Per Hour":
         ax.plot(df["simulationTime[s]"], df["averageInterventionDuration[min]"],
                 label="Average Intervention Duration [min]")
@@ -77,25 +105,46 @@ def create_plot(selected_date, selected_chart_title):
         fig.legend(loc="upper right")
 
     elif selected_chart_title == "Average Duration Patrols Heading Towards Incidents Per Hour":
-        ax.plot(df["simulationTime[s]"], df["averageTransferToInterventionDuration[s]"],
+
+        ax.plot(df["simulationTime[s]"] / 3600, df["averageTransferToInterventionDuration[s]"],
                 label="Average Transfer to Intervention Duration [s]")
         ax.set_title("Average Duration Patrols Heading Towards Incidents Per Hour")
-        ax.set_xlabel("Time [s]")
+
+        # Ustawienia osi i etykiet
+        ax.set_xlim(left = 0)  # Minimalna wartość na osi x
+        ax.set_ylim(bottom = 0)  # Minimalna wartość na osi y
+        ax.xaxis.set_major_locator(MultipleLocator(1))  # Przyrost co 1 jednostkę na osi x
+        ax.set_xlabel("Time [h]")
         ax.set_ylabel("Average Transfer to Intervention Duration [s]")
+        
         ax2 = ax.twinx()
-        ax2.plot(df["simulationTime[s]"], df["averageTransferToFiringDuration[s]"], color='orange',
+        
+        ax2.plot(df["simulationTime[s]"] / 3600, df["averageTransferToFiringDuration[s]"], color='orange',
                  label="Average Transfer to Firing Duration [s]")
+        
+        # Ustawienia osi i etykiet
+        ax2.set_xlim(left = 0)  # Minimalna wartość na osi x
+        ax2.set_ylim(bottom = 0)  # Minimalna wartość na osi y
+        ax2.xaxis.set_major_locator(MultipleLocator(1))  # Przyrost co 1 jednostkę na osi x
         ax2.set_ylabel("Average Transfer to Firing Duration [s]")
         fig.legend(loc="upper right")
 
     elif selected_chart_title == "Average Swat Distance And Time To Reach Firing":
-        ax.plot(df["simulationTime[s]"], df["averageDistanceToReach[m]"],
+        ax.plot(df["simulationTime[s]"] / 3600, df["averageDistanceToReach[m]"],
                 label="Average SWAT Distance to Reach [m]")
+        
+        # Ustawienia osi i etykiet
+        ax.set_xlim(left = 0)  # Minimalna wartość na osi x
+        ax.set_ylim(bottom = 0)  # Minimalna wartość na osi y
+        ax.xaxis.set_major_locator(MultipleLocator(1))  # Przyrost co 1 jednostkę na osi x
         ax.set_title("Average Swat Distance And Time To Reach Firing")
-        ax.set_xlabel("Time [s]")
+        ax.set_xlabel("Time [h]")
         ax.set_ylabel("Average SWAT Distance to Reach [m]")
+
         ax2 = ax.twinx()
-        ax2.plot(df["simulationTime[s]"], df["averageTimeToReach[s]"], color='orange', label="Average SWAT Time to Reach [s]")
+        
+        # Ustawienia osi i etykiet
+        ax2.plot(df["simulationTime[s]"] / 3600, df["averageTimeToReach[s]"], color='orange', label="Average SWAT Time to Reach [s]")
         ax2.set_ylabel("Average SWAT Time to Reach [s]")
         fig.legend(loc="upper right")
 
